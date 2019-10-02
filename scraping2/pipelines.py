b1ac7e017e38bd38e858d0798824a5ae690d6c33
kenneth irungu <kennethirungu1800@gmail.com>
2019-10-02 06:51:40 -0400

    connected the crawld data to mongo db

--- scraping2/pipelines.py
+++ scraping2/pipelines.py
@@ -4,8 +4,34 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymongo
 
 class Scraping2Pipeline(object):
+    collection_name = 'usproxy'
+
+    def __init__(self):
+         self.mongo_uri = 'mongodb://localhost:27017'
+         self.mongo_db = 'usproxy'
+        
+
+    
+    def open_spider(self, spider):
+         self.client = pymongo.MongoClient(self.mongo_uri)
+         self.db = self.client[self.mongo_db]
+       
+    
+    # @classmethod
+    # def from_crawler(cls, crawler):
+    #     # return cls(
+    #     #     mongo_uri=crawler.settings.get('MONGO_URI'),
+    #     #     mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+    #     # )
+    #     pass
+
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
         return item
--- scraping2/settings.py
+++ scraping2/settings.py
@@ -66,9 +66,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scraping2.pipelines.Scraping2Pipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scraping2.pipelines.Scraping2Pipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
